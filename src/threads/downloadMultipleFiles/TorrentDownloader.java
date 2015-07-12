1fe8e82cbe39b47afaedb435cc172952416b8a2b
Chiranjeevi <chiranjeevi.tapal@outlook.com>
2015-07-12 17:45:37 +0530

    my own torrent downloader

---
+++ src/threads/downloadMultipleFiles/TorrentDownloader.java
@@ -0,0 +1,24 @@
+package threads.downloadMultipleFiles;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import com.turn.ttorrent.client.Client;
+import com.turn.ttorrent.client.SharedTorrent;
+
+public class TorrentDownloader{
+	public static void main(String args[]) throws UnknownHostException, IOException{
+		File srcTorrentFile = new File("C://Users/Chiranjeevi/Desktop/[kat.cr]fast.and.furious.7.official.trailer.1080p.hd.headbangersfc.torrent");
+		File destinationFile = new File("C://Users/Chiranjeevi/Desktop");
+		System.out.println("srcTorrentFile "+srcTorrentFile.exists()+" dest "+destinationFile.exists());
+		InetAddress inetAddress = InetAddress.getLocalHost();
+		SharedTorrent sharedTorrent = SharedTorrent.fromFile(srcTorrentFile, destinationFile);
+		Client client = new Client(inetAddress, sharedTorrent);
+		client.setMaxDownloadRate(100000.0);
+		client.setMaxUploadRate(10.0);
+		client.download();
+		client.waitForCompletion();
+	}
+}
