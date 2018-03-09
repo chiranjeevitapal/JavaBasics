1076e5af5000e5e70da85b838217ccfeb4b28f33
Chiranjeevi Tapal <ctapal@qti.qualcomm.com>
2018-03-09 15:50:10 +0530

    new

--- src/amazon/PivotOfANumber.java
+++ src/amazon/PivotOfANumber.java
@@ -1,8 +1,11 @@
 package amazon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PivotOfANumber {
 	public static void main(String args[]) {
-		String number = "92";
+		String number = "101123";
 		String pivot = "";
 		String leftPart = "";
 		String rightPart = "";
@@ -19,19 +22,26 @@ public class PivotOfANumber {
 					palindrome = (Integer.parseInt(number) - 10) + "";
 				}
 			} else {
-				String smallPivot = pivot.replace(pivot.charAt(0), pivot.charAt(1));
-				String bigPivot = pivot.replace(pivot.charAt(1), pivot.charAt(0));
-				if (Math.abs(Integer.parseInt(pivot) - Integer.parseInt(smallPivot)) < Math
-						.abs(Integer.parseInt(pivot) - Integer.parseInt(bigPivot))) {
-					pivot = smallPivot;
-				} else {
-					pivot = bigPivot;
+				List<Integer> pivotPal = new ArrayList<>();
+				for (int i = Integer.parseInt(pivot) - 10; i < Integer.parseInt(pivot) + 10; i++) {
+					if (checkIfPalindrome(i + "")) {
+						pivotPal.add(i);
+					}
+				}
+				int prevDiff = 100;
+				for (int elem : pivotPal) {
+					if ( Math.abs(Integer.parseInt(pivot) - elem) < prevDiff) {
+						prevDiff =  Math.abs(Integer.parseInt(pivot) - elem);
+						pivot = elem + "";
+					}
 				}
 				StringBuffer buffer = new StringBuffer(rightPart);
 				palindrome = leftPart + pivot + (buffer.reverse().toString());
 			}
 
-		} else {
+		} else
+
+		{
 			pivot = number.charAt(number.length() / 2) + "";
 			leftPart = number.substring(0, number.length() / 2);
 			rightPart = number.substring(number.length() / 2 + 1);
